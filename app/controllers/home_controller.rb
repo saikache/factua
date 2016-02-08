ee5305d5f327f90bd5a3bd957a59e2262f4e9591
kachesai <saisagarkache@gmail.com>
2016-02-08 23:57:12 +0530

    search for factula

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
+  require 'factual'
   def index
-
-
-
-
-  	
+    # byebug
+    if params['get_data'].present?
+      factual = Factual.new("Dx0l48S0qhMLi89bM2B8Lmw3jaWx2sBwlVeabK5h", "eKqeP0bLxaVB70AEcMbZhsV98ypoyQVT2oEThXeu")
+      @result =  factual.table("places-us").search(params['get_data'])
+    end
+    
   end
 end
